83f16dcd9af4baa7f68e0ca919af21ba342343dd
woojoung <chae3229@gmail.com>
2024-01-17 15:14:51 +0900

    fix: userSession 코드 rollback

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -13,7 +13,6 @@ export default function Component() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  const [userSession, setUserSession] = useState<any>(null)
   const [user, setUser] = useState({name: ''});
   const [users, setUsers] = useState([]);
   const [total, setTotalFrequency] = useState(0);
@@ -29,7 +28,7 @@ export default function Component() {
     console.log('users: ', users)
     setUsers(users)
 
-    const user: any = users.find((u: any) => u.name === userSession?.user?.name);
+    const user: any = users.find((u: any) => u.name === session?.user?.name);
 
     if (user){
       setUser(user);
@@ -44,18 +43,14 @@ export default function Component() {
   };
 
   useEffect(() => {
-    setUserSession(session)
-  }, [])
-
-  useEffect(() => {
     // 페이지가 마운트될 때 세션을 확인
-    if (typeof userSession === 'undefined') {
+    if (typeof session === 'undefined') {
       // 세션 정보가 없으면 로그인 페이지로 리디렉션
       alert('세션 만료')
       router.push('/');
     }
     findUsers();
-  }, [userSession, router]);
+  }, [session, router]);
 
   
   // console.log('session?.user?.name', JSON.stringify(session));
@@ -103,7 +98,7 @@ export default function Component() {
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center">
           <h1 className="mt-4 text-center text-3xl font-bold" style={{color: '#0C3659'}}>Wave Maker</h1>
           <p className="mt-2 text-center text-lg text-gray-600" style={{color: '#0C3659'}}>Break Time</p>
-          <h1 className="mt-4 text-center text-3xl font-bold" style={{color: '#0C3659'}}>{userSession ? `${user.name}` : ''}</h1>
+          <h1 className="mt-4 text-center text-3xl font-bold" style={{color: '#0C3659'}}>{session ? `${user.name}` : ''}</h1>
         </div>
         
       </div>
