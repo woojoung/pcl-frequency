9ce205d84db25f727c876566b2d1fa73e933cb8c
Woojoung <chae3229@gmail.com>
2024-01-20 01:49:14 +0900

    feat: admin login 추가

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -15,6 +15,7 @@ export default function Component() {
   const [score, setRoundScore] = useState('');
   const [users, setUsers] : any = useState([]);
   // const [loading, setLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
 
   const findUsers = async () => {
     const usersRef = query(collection(firestore, "users"), orderBy("id", "asc"))
@@ -23,6 +24,21 @@ export default function Component() {
     console.log('users: ', users);
     setUsers(users);
     // setLoading(false);
+  };
+
+  useEffect(() => {
+    if (refreshing) {
+      // Your refresh logic here
+      findUsers();
+
+      // Reset the refresh status after handling the refresh
+      setRefreshing(false);
+    }
+  }, [refreshing]);
+
+  // Function to trigger the refresh
+  const handleRefresh = () => {
+    setRefreshing(true);
   };
 
   useEffect(() => {
@@ -69,7 +85,7 @@ export default function Component() {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-lg font-semibold" style={{color: '#0C3659'}}>Wave Maker Frequency</h1>
         {/* <SettingsIcon className="text-gray-600" /> */}
-        <RefreshIcon className="text-gray-600 hover:bg-sky-300 hover:rounded" onClick={findUsers}/>
+        <RefreshIcon className="text-gray-600 hover:bg-sky-300 hover:rounded" onClick={handleRefresh}/>
       </div>
       <div className="mb-2">
         <span className="text-sm font-medium" style={{color: '#0C3659'}}>Admin</span>
@@ -203,7 +219,7 @@ function RefreshIcon({ color, ...props }: any) {
   return (
     <button className="flex items-center px-2 py-3 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-sky-600 rounded-lg hover:bg-sky-500 focus:outline-none focus:ring focus:ring-sky-300 focus:ring-opacity-80">
       <svg className="w-5 h-5 mx-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clip-rule="evenodd" />
+          <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
       </svg>
 
       <span className="mx-1">Refresh</span>
